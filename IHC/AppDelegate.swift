388edbca7280f600319864045aaf1a7dd6158d30
ezequieloliveiralima <ezequieldeoliveiralima@gmail.com>
2016-11-20 13:33:23 -0200

    algumas mudancas

--- IHC/AppDelegate.swift
+++ IHC/AppDelegate.swift
@@ -40,6 +40,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+    }
 
 
 }
